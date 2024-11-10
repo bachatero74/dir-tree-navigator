81a1f435fda5606abb3421ef2b3837b5e6455289
jacek <jacek.ruszecki@gmail.com>
2024-11-10 18:31:06 +0100

    Screen

--- navigator/src/app_sys.rs
+++ navigator/src/app_sys.rs
@@ -7,5 +7,4 @@ pub enum AppError {
 
     #[error("Błąd IO: {0}")]
     IoError(#[from] std::io::Error),
-
 }
--- navigator/src/main.rs
+++ navigator/src/main.rs
@@ -1,5 +1,70 @@
 mod app_sys;
+use ncurses::*;
+
+struct Screen {
+    left_pane: WINDOW,
+    right_pane: WINDOW,
+    tree_win: WINDOW,
+    list_win: WINDOW,
+}
+
+impl Screen {
+    fn initialize() -> Screen {
+        initscr();
+        cbreak();
+        noecho();
+        refresh();
+
+        let mut scr_height: i32 = 0;
+        let mut scr_width: i32 = 0;
+
+        getmaxyx(ncurses::stdscr(), &mut scr_height, &mut scr_width);
+
+        let l_width = scr_width / 4;
+        let r_width = scr_width - l_width;
+
+        // Tree
+        let left_pane = newwin(scr_height, l_width, 0, 0);
+        box_(left_pane, 0, 0);
+        wrefresh(left_pane);
+
+        let tree_win: WINDOW = newwin(scr_height - 2, l_width - 2, 1, 1);
+        wrefresh(tree_win);
+
+        // List
+        let right_pane = newwin(scr_height, r_width, 0, l_width);
+        box_(right_pane, 0, 0);
+        wrefresh(right_pane);
+
+        let list_win: WINDOW = newwin(scr_height - 2, r_width - 2, 1, l_width + 1);
+        wrefresh(list_win);
+
+        Screen {
+            left_pane,
+            right_pane,
+            tree_win,
+            list_win,
+        }
+    }
+
+    fn close(&self) {
+        delwin(self.tree_win);
+        delwin(self.list_win);
+        delwin(self.left_pane);
+        delwin(self.right_pane);
+        endwin();
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen::initialize();
+    getch();
+    
+    mvwprintw(screen.tree_win, 0, 0, "123456789");
+    mvwprintw(screen.list_win, 0, 0, "123456789");
+    wrefresh(screen.tree_win);
+	wrefresh(screen.list_win);
+
+    getch();
+    screen.close();
 }
