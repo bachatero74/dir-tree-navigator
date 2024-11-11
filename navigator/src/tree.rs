85de78bcf222bda317e75f6298cd9584b9fa2fcc
jacek <jacek.ruszecki@gmail.com>
2024-11-11 07:29:12 +0100

    Tree Items added

--- navigator/src/tree.rs
+++ navigator/src/tree.rs
@@ -1,1 +1,19 @@
+
+pub struct Node{
+
+}
+
+pub struct FileNode {
+    pub node:Node,
+}
+
+pub struct DirNode {
+    pub node:Node,
+}
+
+pub enum TreeNode {
+    File(FileNode),
+    Dir(DirNode),
+}
+
 pub struct Tree {}
