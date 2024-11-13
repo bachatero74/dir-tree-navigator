2e0e11c52541dab3e73a48b55095b395d7827004
jacek <jacek.ruszecki@gmail.com>
2024-11-13 13:13:33 +0100

    _

--- navigator/src/graph/tree_view.rs
+++ navigator/src/graph/tree_view.rs
@@ -28,6 +28,13 @@ impl DisplContent for TreeView {
         let tree = self.tree.borrow();
         info.lines_count = tree.tmp_lines.len() as i32;
         info.curs_line = tree.tmp_cursor;
+        match tree.tmp_lines.get(tree.tmp_cursor as usize) {
+            Some(line) => {
+                info.curs_x1=line.x1;
+                info.curs_x2=line.x2;
+            },
+            None => return Err(AppError::StrError("Index out of range".to_owned())),
+        }
         for i in 0..info.lines_count {
             let src: &ViewLine = &tree.tmp_lines[i as usize];
             self.lines
